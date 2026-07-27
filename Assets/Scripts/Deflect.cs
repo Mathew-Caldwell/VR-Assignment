91de7ed37d74f8b6519899d877416576558881fa
Mathew-Caldwell <20143330@tafe.wa.edu.au>
2026-07-27 22:19:40 +0800

    added deflect sound effects

--- Assets/Scripts/Deflect.cs
+++ Assets/Scripts/Deflect.cs
@@ -3,12 +3,22 @@ using UnityEngine;
 public class Deflect : MonoBehaviour
 {
     int numDeflected = 0;
-    private void OnCollisionEnter(Collision collision)
+
+    private AudioSource audioSource;
+    public AudioClip deflectSound;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Bolt")){
-            numDeflected++;
+        AudioSource.PlayClipAtPoint(deflectSound, transform.position);
+        numDeflected++;
             Debug.Log($"Number deflected: {numDeflected}");
             Destroy(collision.gameObject);
+            
         }
     }
 }
