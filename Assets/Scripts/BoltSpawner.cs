d19f7d123602df501538d7493870b1dd45572ca1
Mathew-Caldwell <20143330@tafe.wa.edu.au>
2026-07-28 17:19:55 +0800

    changed it so instead of the bolt becoming faster the spawn time decreases

--- Assets/Scripts/BoltSpawner.cs
+++ Assets/Scripts/BoltSpawner.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class BoltSpawner : MonoBehaviour
 {
-    [Header("Bolt Prefab")]
+    [Header("Bolt Data")]
     public GameObject bolt;
-    public float waitTime = 5f;
+    public float waitTime = 1f;
+    public float incrementSpawnSpeed = 0.1f;
+    public float timeTillNextIncrement = 5f;
+    float nextTime = 0f;
     float endTime = 0f;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         endTime = Time.time + waitTime;
+        nextTime = Time.time + timeTillNextIncrement;
     }
 
     // Update is called once per frame
@@ -18,6 +22,11 @@ public class BoltSpawner : MonoBehaviour
     {
         if (Time.time > endTime)
         {
+            if(Time.time > nextTime)
+            {
+                waitTime -= incrementSpawnSpeed;
+                nextTime = Time.time + timeTillNextIncrement;
+            }
             Instantiate(bolt, new Vector3(transform.position.x, transform.position.y + Random.Range(-0.5f, 0.5f), transform.position.z + Random.Range(-1f, 1f)), Quaternion.Euler(0f, 0f, 90f));
             endTime = Time.time + waitTime;
 
