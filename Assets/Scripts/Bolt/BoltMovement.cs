e6ca0dbbd8b364387ce53d73c36914f6a817232a
Mathew-Caldwell <20143330@tafe.wa.edu.au>
2026-08-31 12:43:07 +0800

    disables the child colliders on the bolts as they were causing 3x collision instead of one

--- Assets/Scripts/Bolt/BoltMovement.cs
+++ Assets/Scripts/Bolt/BoltMovement.cs
@@ -10,42 +10,51 @@ public class BoltMovement : MonoBehaviour
     // 10 is easy, 20 medium,
     float speed;
 
+    Rigidbody rb;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         player = GameObject.FindWithTag("Player");
         spawner = GameObject.FindWithTag("Spawner");
+
         speed = spawner.GetComponent<BoltSpawner>().move;
+
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(spawner.GetComponent<BoltSpawner>().transform.forward * speed * 4000);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (player.GetComponent<PauseMenu>().isVisible)
-        {
-            speed = 0;
-        }
-        else if (isDeflected)
-        {
-            speed = spawner.GetComponent<BoltSpawner>().move * -1;
-        }
-        else
-        {
-            speed = spawner.GetComponent<BoltSpawner>().move;
-        }
-
-        Vector3 direction = new Vector3(0, 1, 0);
-
-        transform.Translate(direction * speed * Time.deltaTime);
+        
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("LaserSword"))
         {
-            isDeflected = true;
             Destroy(GetComponent<Collider>());
             
+            //stops the bolt from moving in any direction other than the x axis
+            rb.constraints = RigidbodyConstraints.FreezePositionY;
+            rb.constraints = RigidbodyConstraints.FreezePositionZ;
+
+            //stops the bolt from randomly spinning
+            rb.freezeRotation = true;
+
+            //disables further collisions
+            rb.detectCollisions = false;
+
+            //invertes the velocity of the bolt
+            rb.linearVelocity = Vector3.zero;
+            rb.AddForce(spawner.GetComponent<BoltSpawner>().transform.forward * speed * 2000 * -1);            
+        }
+        else if (collision.gameObject.CompareTag("Player"))
+        {
+            //disables further collisions
+            Destroy(GetComponent<Collider>());
+            rb.detectCollisions = false;
         }
     }
 }
